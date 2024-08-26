512557636f45f019105dcede19564df05f377810
mariobp-NHH <78204796+mariobp-NHH@users.noreply.github.com>
2024-08-26 10:55:55 +0200

    Add files via upload

---
+++ Cap 5. Modelo linguistico y Bases de Datos en AWS/Seccion 1b - Boilerplate/capp/light_talk_app/forms.py
@@ -0,0 +1,28 @@
+from flask_wtf import FlaskForm
+from wtforms import  SubmitField,  SelectField,  FloatField, StringField
+from wtforms.validators import InputRequired
+
+class AddForm(FlaskForm):
+  incorrect_sentence = StringField('Frase Incorrecta', [InputRequired()])
+  correct_sentence_one = StringField('Frase Correcta 1', [InputRequired()], default="none")
+  correct_sentence_two = StringField('Frase Correcta 2', [InputRequired()], default="none")
+  correct_sentence_three = StringField('Frase Correcta 3', [InputRequired()], default="none")
+  objective = SelectField('Objetivo', [InputRequired()], 
+    choices=[('Genero', 'Genero'), ('Número', 'Número')])
+  source = SelectField('Fuente', [InputRequired()], 
+    choices=[('Inteligencia Artificial', 'Inteligencia Artificial'), ('Persona', 'Persona'), ('Lingüista', 'Lingüista')])
+  submit = SubmitField('Añadir')
+
+class SelectForm(FlaskForm):
+  objective = SelectField('Objetivo', [InputRequired()], 
+    choices=[('Genero', 'Genero'), ('Número', 'Número'), ('Otro', 'Otro')])
+  source = SelectField('Fuente', [InputRequired()], 
+    choices=[('Inteligencia Artificial', 'Inteligencia Artificial'), ('Persona', 'Persona'), ('Lingüista', 'Lingüista')])
+  submit = SubmitField('Seleccionar')
+
+class VerifyForm1(FlaskForm):
+  incorrect_sentence = StringField('Frase Incorrecta', [InputRequired()])
+  your_sentence = StringField('¿Cuál es la frase correcta?', [InputRequired()])
+  objective = StringField('Objetivo', [InputRequired()])
+  source = StringField('Fuente', [InputRequired()])
+  submit = SubmitField('Verifica')
